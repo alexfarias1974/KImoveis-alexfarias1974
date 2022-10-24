c7e0259518aeb302f37ba1f781276394b183c9d7
alex_wfarias <alex_wfarias@yahoo.com.br>
2022-10-24 18:55:57 -0400

    fix: migrations files

---
+++ src/migrations/1666651115390-bugRelationUser.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class bugRelationUser1666651115390 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
